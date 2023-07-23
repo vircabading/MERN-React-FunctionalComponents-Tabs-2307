d5852c96d74799abca556009c32dc41f9bcd56b1
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-07-23 00:01:08 -0500

    add color pallate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Tabs App</h1>
-    </div>
+    <main>
+      <div className='container mt-4 p-2 bg-white text-dark-color  round'>
+        <h1 >Tabs App</h1>
+
+      </div>
+
+    </main>
   );
 }
 

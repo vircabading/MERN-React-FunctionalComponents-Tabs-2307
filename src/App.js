abf32246d0e3f636bbc1593e6b63cf9a72113f0c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-07-29 23:06:44 -0500

    imlement navbar view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import NavBarView from './components/NavBarView';
+
+///////////////////////////////////////////////////
+// MAIN APPLICATION
+///////////////////////////////////////////////////
 
 function App() {
   return (
     <main>
+      <NavBarView />
       <div className='container mt-4 p-2 bg-white text-dark-color  round'>
         <h1 >Tabs App</h1>
 
